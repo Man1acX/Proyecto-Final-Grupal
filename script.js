b01b96468f4e0011fbccf294ee93a0e3d69c2377
Man1acX <ale_0_lfb@hotmail.com>
2023-11-07 20:20:15 -0300

    script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const loaderEl = document.querySelector(".loader");
 const wrapperEl = document.querySelector(".wrapper");
 const dataEl = document.getElementById("search-box");
 
-generarGif = () => {
+const generarGif = () => {
     loaderEl.style.display = "block";
     wrapperEl.style.display = "none";
 
@@ -13,36 +13,60 @@ generarGif = () => {
     const Count = 50;
 
     const URL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${dato}&limit=${Count}&offset=0&rating=g&lang=en`;
+    document.querySelector(".wrapper").innerHTML = "";
 
-    function llamado(){
-        fetch(URL)
-            .then((Response) => Response.json())
-            .then((info) => {
-                console.log(info.data)
+    fetch(URL)
+        .then((Response) => Response.json())
+        .then((info) => {
+            console.log(info.data);
+            const dataGif = info.data;
+            dataGif.forEach((gif) => {
+                const container = document.createElement("div");
+                container.classList.add("container");
 
-                const dataGif = info.data;
-                dataGif.forEach((gif) => {
-                    const container = document.createElement("div");
-                    container.classList.add("container");
+                const image = document.createElement("img");
+                console.log(gif);
 
-                    const image = document.createElement("img");
-                    console.log(gif);
-
-                    image.setAttribute("src", gif.image.downsized_medium.url);
-                    image.onload = () => {
-                        Count--;
-                        if(Count == 0){
-                            loaderEl.style.display = "none";
-                            document.querySelector("wrapper").style.display = "grid";
-                        }
-                    };
-                    container.append(image);
+                image.setAttribute("src", gif.image.downsized_medium.url);
+                image.onload = () => {
+                    Count--;
+                    if(Count == 0){
+                        loaderEl.style.display = "none";
+                        document.querySelector("wrapper").style.display = "grid";
+                    }
+                };
+                container.append(image);
 
 
-                    const copiar = document.createElement("button");
-                    copiar.innerText = "Copy";
-                });
-            })
-    }
-}
+                const copiar = document.createElement("button");
+                copiar.innerText = "Copy";
+                copiar.onclick = () => {
+
+                    const copyurl = `https://media4.giphy.com/media/${gif.id}/giphy.mp4`;
+
+                    navigator.clipboard
+                        .writeText(copyurl)
+                        .then(() => {
+                            alert("Gif copied to clipboard");
+                        })
+                        .catch(() => {
+                            alert("Gif copied to clipboard");
+
+                            const hiddenInput = document.createElement("input");
+                            hiddenInput.setAttribute("type", "text");
+                            document.body.appendChild(hiddenInput);
+                            hiddenInput.value = copyurl;
+                            hiddenInput.select();
+                            document.execCommand("copy");
+                            document.body.removeChild(hiddenInput);
+                        });
+                };
+                container.append(copiar);
+                document.querySelector(".wrapper").append(container);
+            });
+        });
+};
+
+submitEl.addEventListener("click", generarGif);
+window.addEventListener("load", generarGif);
 
